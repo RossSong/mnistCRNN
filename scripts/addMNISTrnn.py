6cfeff0091f2efde9b53ea9f0a750865612f8bfa
James M Frick <james.m.frick@kp.org>
2017-03-07 10:57:08 -0500

    fix dim ordering for new tf default

--- scripts/addMNISTrnn.py
+++ scripts/addMNISTrnn.py
@@ -7,7 +7,7 @@ Learning is based on the sum of the digits, not explicit labels on each digit.
 from __future__ import print_function
 import numpy as np
 
-
+import keras.backend as K
 from keras.datasets import mnist
 from keras.models import Sequential
 #from keras.initializations import norRemal, identity
@@ -21,7 +21,7 @@ from keras.models import model_from_json
 
 # for reproducibility
 np.random.seed(2016)  
-
+K.set_image_dim_ordering('tf')
 #define some run parameters
 batch_size      = 32
 nb_epochs       = 20
@@ -54,7 +54,7 @@ print("Building model")
 
 #define our time-distributed setup
 model = Sequential()
-model.add(TimeDistributed(Convolution2D(8, 4, 4, border_mode='valid'), input_shape=(maxToAdd,1,size,size)))
+model.add(TimeDistributed(Convolution2D(8, 4, 4, border_mode='valid'), input_shape=(maxToAdd,size,size,1)))
 model.add(Activation('relu'))
 model.add(TimeDistributed(Convolution2D(16, 3, 3, border_mode='valid')))
 #model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2),border_mode='valid')))
@@ -79,19 +79,19 @@ for ep in range(0,nb_epochs):
     X_test        = []
     y_test        = []    
     
-    X_train     = np.zeros((examplesPer,maxToAdd,1,size,size))
+    X_train     = np.zeros((examplesPer,maxToAdd,size,size,1))
 
     for i in range(0,examplesPer):
         #initialize a training example of max_num_time_steps,im_size,im_size
-        output      = np.zeros((maxToAdd,1,size,size))
+        output      = np.zeros((maxToAdd,size,size,1))
         #decide how many MNIST images to put in that tensor
-        numToAdd    = np.ceil(np.random.rand()*maxToAdd)
+        numToAdd    = int(np.ceil(np.random.rand()*maxToAdd))
         #sample that many images
         indices     = np.random.choice(X_train_raw.shape[0],size=numToAdd)
         example     = X_train_raw[indices]
         #sum up the outputs for new output
         exampleY    = y_train_temp[indices]
-        output[0:numToAdd,0,:,:] = example
+        output[0:numToAdd,:,:,0] = example
         X_train[i,:,:,:,:] = output
         y_train.append(np.sum(exampleY))
 
@@ -105,25 +105,25 @@ for ep in range(0,nb_epochs):
               verbose=1)
 
 #Test the model
-X_test     = np.zeros((examplesPer,maxToAdd,1,size,size))
-for i in range(0,examplesPer):
-    output      = np.zeros((maxToAdd,1,size,size))
-    numToAdd    = np.ceil(np.random.rand()*maxToAdd)
-    indices     = np.random.choice(X_test_raw.shape[0],size=numToAdd)
-    example     = X_test_raw[indices]
-    exampleY    = y_test_temp[indices]
-    output[0:numToAdd,0,:,:] = example
-    X_test[i,:,:,:,:] = output
-    y_test.append(np.sum(exampleY))
-
-X_test  = np.array(X_test)
-y_test  = np.array(y_test)       
-
-preds   = model.predict(X_test)
-
-#print the results of the test    
-print(np.sum(np.sqrt(np.mean([ (y_test[i] - preds[i][0])**2 for i in range(0,len(preds)) ]))))
-print("naive guess", np.sum(np.sqrt(np.mean([ (y_test[i] - np.mean(y_test))**2 for i in range(0,len(y_test)) ]))))
+#X_test     = np.zeros((examplesPer,maxToAdd,1,size,size))
+#for i in range(0,examplesPer):
+#    output      = np.zeros((maxToAdd,1,size,size))
+#    numToAdd    = np.ceil(np.random.rand()*maxToAdd)
+#    indices     = np.random.choice(X_test_raw.shape[0],size=numToAdd)
+#    example     = X_test_raw[indices]
+#    exampleY    = y_test_temp[indices]
+#    output[0:numToAdd,0,:,:] = example
+#    X_test[i,:,:,:,:] = output
+#    y_test.append(np.sum(exampleY))
+#
+#X_test  = np.array(X_test)
+#y_test  = np.array(y_test)       
+#
+#preds   = model.predict(X_test)
+#
+##print the results of the test    
+#print(np.sum(np.sqrt(np.mean([ (y_test[i] - preds[i][0])**2 for i in range(0,len(preds)) ]))))
+#print("naive guess", np.sum(np.sqrt(np.mean([ (y_test[i] - np.mean(y_test))**2 for i in range(0,len(y_test)) ]))))
 
 #save the model
 #jsonstring  = model.to_json()
